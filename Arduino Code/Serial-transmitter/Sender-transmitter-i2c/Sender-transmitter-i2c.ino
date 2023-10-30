0bc8814042733bea6f852734770484be38fce510
DallinatorX <dallinhatton@gmail.com>
2023-10-29 21:14:18 -0600

    Fixed keyboard comunication errors

---
+++ Arduino Code/Serial-transmitter/Sender-transmitter-i2c/Sender-transmitter-i2c.ino
@@ -0,0 +1,19 @@
+#include <Wire.h>
+
+void setup() {
+  Wire.begin(); // Initialize I2C as Master
+  Serial.begin(9600); // Initialize Serial communication
+}
+
+void loop() {
+  // Read data from Serial and send it over I2C
+  if (Serial.available()) {
+    
+    Wire.beginTransmission(9); // Address of the receiver Arduino
+    while (Serial.available()){
+      char data = Serial.read();
+      Wire.write(data);
+    }
+    Wire.endTransmission();
+  }
+}
--- Examples/getkeyboardinput.py
+++ Examples/getkeyboardinput.py
@@ -4,6 +4,8 @@ from pynput.keyboard import Key, Listener
 
 # Keep track of keys that are currently held down
 current_keys = set()
+ser1 = serial.Serial("/dev/ttyACM2",9600)
+
 
 # Define functions for keypress and key release events
 def on_press(key):
@@ -12,7 +14,7 @@ def on_press(key):
         # Remove "Key." prefix and single quotes, if present
         key_str = str(key).replace("Key.", "").strip("'")
         print(f"Key {key_str} pressed")
-        sendKeyboardMouseAction('keydown',key_str,0,0)
+        sendKeyboardMouseAction('keydown',key_str,0,0,ser1)
 
 def on_release(key):
     if key in current_keys:
@@ -20,7 +22,7 @@ def on_release(key):
         # Remove "Key." prefix and single quotes, if present
         key_str = str(key).replace("Key.", "").strip("'")
         print(f"Key {key_str} released")
-        sendKeyboardMouseAction('keyup',key_str,0,0)
+        sendKeyboardMouseAction('keyup',key_str,0,0,ser1)
         
 
 # Create keyboard listeners
