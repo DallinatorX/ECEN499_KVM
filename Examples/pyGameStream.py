4aa4cc7f953c200eb2f1294bf2bd205e62223a55
DallinatorX <dallinhatton@gmail.com>
2023-11-13 13:07:33 -0700

    Added menu buttons

--- Examples/pyGameStream.py
+++ Examples/pyGameStream.py
@@ -1,12 +1,13 @@
 import cv2
 import pygame
+import pygame_gui
 from pygame.locals import *
 
-device = "video2"
+device = "video0"
 
 
 def getVideoInputDevice():
-    frames_loc = '/dev/'+ str(device)
+    frames_loc = '/dev/' + str(device)
     return frames_loc
 
 
@@ -20,42 +21,68 @@ screen = pygame.display.set_mode((window_width, window_height))
 width = 1920
 height = 1080
 
-
-
 # Initialize OpenCV for video capture
-cap = cv2.VideoCapture(getVideoInputDevice()) #'/dev/video2'
-cap.set(3,width)
-cap.set(4,height)
+cap = cv2.VideoCapture(getVideoInputDevice())
+cap.set(3, width)
+cap.set(4, height)
 
+paused = False  # Variable to keep track of the pause state
 
+# Initialize Pygame GUI
+manager = pygame_gui.UIManager((window_width, window_height))
+
+# Create a button
+pause_button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((10, 10), (100, 50)),
+                                            text='Pause',
+                                            manager=manager)
+
+hi_button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((120, 10), (100, 50)),
+                                            text='Hi',
+                                            manager=manager)
 
 while True:
+    time_delta = pygame.time.Clock().tick(60) / 1000.0
+
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
+        elif event.type == KEYDOWN:
+            if event.key == K_TAB and pygame.key.get_mods() & KMOD_SHIFT:
+                paused = not paused  # Toggle pause state
+        elif event.type == pygame.USEREVENT:
+            if event.user_type == pygame_gui.UI_BUTTON_PRESSED:
+                if event.ui_element == pause_button:
+                    paused = not paused  # Toggle pause state
+                if event.ui_element == hi_button:
+                    print("Hi")  # Toggle pause state
 
+        manager.process_events(event)
 
-    # Read a frame from the video stream
-    ret, frame = cap.read()
-    if not ret:
-        break
+    if not paused:
+        # Read a frame from the video stream
+        ret, frame = cap.read()
+        if not ret:
+            break
 
-    # Rotate the frame 90 degrees clockwise
-    frame = cv2.rotate(frame, cv2.ROTATE_90_COUNTERCLOCKWISE)
+        # Rotate the frame 90 degrees clockwise
+        frame = cv2.rotate(frame, cv2.ROTATE_90_COUNTERCLOCKWISE)
 
-    # Resize the frame to double the size
-    #frame = cv2.resize(frame, (frame.shape[1] * 2, frame.shape[0] * 2))
+        # Convert the frame to RGB (Pygame uses RGB format)
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
-    # Convert the frame to RGB (Pygame uses RGB format)
-    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        # Convert the frame to Pygame surface
+        frame = pygame.surfarray.make_surface(frame)
 
-    # Convert the frame to Pygame surface
-    frame = pygame.surfarray.make_surface(frame)
+        # Blit the frame to the Pygame window
+        screen.blit(frame, (0, 0))
+    else:
+        # Update the Pygame GUI manager
+        manager.update(time_delta)
+        # Draw the GUI manager
+        manager.draw_ui(screen)
 
-    # Blit the frame to the Pygame window
-    screen.blit(frame, (0, 0))
     pygame.display.flip()
 
 cap.release()
-
+pygame.quit()
