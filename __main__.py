99ee92f7a2745ab28e617f97d841c20cd3ef2c78
DallinatorX <dallinhatton@gmail.com>
2023-11-27 15:42:10 -0700

    Added comments

--- __main__.py
+++ __main__.py
@@ -1,9 +1,7 @@
 """
 This is the core file for the KVM. Running this will start all needed function.
 """
-# from Programs.rtsp_v2 import *
 from Programs.getkeyboardinput import *
-from Programs.getMouseInput import *
 import threading
 import subprocess
 import pygame
@@ -16,17 +14,23 @@ import pygame_gui
 #Set Video device info
 device = "video0"
 
+#Set the arduino path
+arduino_path = "/dev/ttyACM0"
+
 # Other constants
 power_code = "p"
 shutdown_code = "k"
 
+# Turn on Verbose
+verbose = False
+
+# Set Screen size
 window_width = 1920
 window_height = 1080
 screen = pygame.display.set_mode((window_width, window_height), pygame.RESIZABLE)
 
 
-#Set the arduino path
-arduino_path = "/dev/ttyACM0"
+
 
 
 # Mouse actions
@@ -34,15 +38,26 @@ leftMouseDown = False
 rightMouseDown = False
 wheelMouseDown = False
 running = True
-paused = False  # Variable to keep track of the pause state
-fullscreen = False # Variable to keep track of the fullscreen state
+
+# Variable to keep track of the pause state
+paused = False  
+
+# Variable to keep track of the fullscreen state
+fullscreen = False 
+
+
 
 def getVideoInputDevice():
+    """
+    Given the video device returns the full path
+    """
     frames_loc = '/dev/' + str(device)
     return frames_loc
 
 def toggle_pause_mode():
-    """Toggles pause-state boolean and visibility of local mouse pointer"""
+    """
+    Toggles pause-state boolean and visibility of local mouse pointer
+    """
     global paused
     paused = not paused  # Toggle pause state
     if paused:
@@ -52,37 +67,55 @@ def toggle_pause_mode():
 
 
 def event_handler():
+    """
+    This thread handles all PyGame events 
+    """
     global leftMouseDown, rightMouseDown, wheelMouseDown, running, fullscreen
 
     while running:
         for event in pygame.event.get():
+            # Exit all functions if Pygame is quit
             if event.type == QUIT:
                 pygame.quit()
                 sys.exit()
                 keyboard_thread.join()
+
+            # Start Pause menu if alt+u is pressed    
             elif event.type == KEYDOWN:
                 if event.key == K_u and pygame.key.get_mods() & KMOD_ALT:
                     toggle_pause_mode() # Toggles pause state
 
+            elif event.type == MOUSEWHEEL:
+                #Recored mouse scroll
+                print(event.x,event.y)
+                sendKeyboardMouseAction("mousescroll",0,event.x, event.y, serial_input)
+
+            # control the GUI
             elif event.type == pygame_gui.UI_BUTTON_PRESSED:
+
                 if event.ui_element == resume_button:
                     print("Host - Resuming...")
                     toggle_pause_mode() # Toggles pause state
+                
                 if event.ui_element == power_button:
                     print("Host - Toggling power button...")
                     serial_input.write(power_code.encode())
+                
                 if event.ui_element == kill_button:
                     print("Host - Forcing shutdown...")
                     serial_input.write(shutdown_code.encode())
+                
                 if event.ui_element == exit_button:
                     print("Disconnecting from Host...")
                     running = False
+                
                 if event.ui_element == fullscreen_button:
                     fullscreen = not fullscreen  # Toggle fullscreen state
                     if fullscreen:
                         screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
                     else:
                         screen = pygame.display.set_mode((window_width, window_height))
+                
                 if event.ui_element == feedback_button:
                     # Open a dialog to get user feedback
                     root = tk.Tk()
@@ -94,19 +127,20 @@ def event_handler():
                         with open('user_feedback.txt', 'a') as file:
                             file.write(feedback + '\n')   
 
-            elif event.type == MOUSEWHEEL:#Recored mouse scroll
-                print(event.x,event.y)
-                sendKeyboardMouseAction("mousescroll",0,event.x, event.y, serial_input)
-
             manager.process_events(event)
 
 def mouse_logger(clock):
+    """
+    Thread to capture mouse movments 
+    """
     global paused, leftMouseDown, rightMouseDown, wheelMouseDown, running
+
     while running:
+
         if not paused:
 
-            mouse_dx, mouse_dy = pygame.mouse.get_rel()
-            mouse_left, mouse_wheel, mouse_right = pygame.mouse.get_pressed()
+            mouse_dx, mouse_dy = pygame.mouse.get_rel() #Get mouse posistion
+            mouse_left, mouse_wheel, mouse_right = pygame.mouse.get_pressed() # Get mouse buttons pressed
 
             # check each button to see if the current state is different from the previous state
             # if the states don't match resolve either the up/down action to return to 
@@ -140,14 +174,15 @@ def mouse_logger(clock):
             # Center the mouse position
             pygame.mouse.set_pos(window_width // 2, window_height // 2)
 
-            # Limit frame rate to 60 FPS
+            # Limit mouse pull rate to 60 FPS
             clock.tick(60)
 
 if __name__ == '__main__':
+    # Set the Serial input
     serial_input = serial.Serial(arduino_path,115200)
 
     # Set up PulseAudio in the OS to listen to the capture card
-    # subprocess.call(['sh', './Programs/pulseAudio_config.sh'])
+    subprocess.call(['sh', './Programs/pulseAudio_config.sh'])
 
 
     # Initialize Pygame
@@ -203,8 +238,7 @@ if __name__ == '__main__':
         # Flip the frame over the y axis
         frame = cv2.flip(frame, 0)
 
-    	# Resize the frame to double the size
-    	# frame = cv2.resize(frame, (frame.shape[1] * 2, frame.shape[0] * 2))
+    	# Resize the frame
         frame = cv2.resize(frame, (window_height,window_width) )
         
         
