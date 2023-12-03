f65dbc78a326ce45e82724d2225d7007b414c684
StorminMormin1 <mikeman825@gmail.com>
2023-12-03 13:01:03 -0700

    cleaned up shell script

--- Programs/pulseAudio_config.sh
+++ Programs/pulseAudio_config.sh
@@ -2,18 +2,15 @@
 #https://stackoverflow.com/questions/3777301/how-to-call-a-shell-script-from-python-code
 #https://unix.stackexchange.com/questions/462670/set-default-profile-for-pulseaudio
 echo "--------CHECKING AUDIO SOURCES--------"
-pactl list short sources # list current available audio sources
+pactl list short sources                        # list current available audio sources
 echo "--------CHECKING ACTIVE AUDIO PROFILES--------"
-pacmd list-cards | grep 'active profile' # lists active audio profiles
-echo "--------SETTING AUDIO DEVICE PROFILE--------"
-# set capture card audio device to active
-echo "--------TRYING TO SET AUDIO DEVICE 2--------"
-pactl set-card-profile 2 input:analog-stereo
+pacmd list-cards | grep 'active profile'        # lists active audio profiles
+echo "-------SETTING AUDIO PROFILE 2 ACTIVE-------"
+pactl set-card-profile 2 input:analog-stereo    # Set capture card audio device 2 to active
 echo "--------TRYING TO SET AUDIO DEVICE 3--------"
-pactl set-card-profile 3 input:analog-stereo #(try again with 3 if 2 is wrong index)
+pactl set-card-profile 3 input:analog-stereo    # (try again with 3 if 2 is wrong index)
 echo "--------SETTING DEFAULT AUDIO SOURCE--------"
 # set default audio source as the capture card
 pactl set-default-source alsa_input.usb-ASUS_CU4K30_UVC_UHD_Video_902B001112901002-02.analog-stereo
 echo "--------MONITORING AUDIO DEVICE--------"
-# load loopback to monitor audio from capture card
-pactl load-module module-loopback
+pactl load-module module-loopback   # load loopback to monitor audio from capture card
